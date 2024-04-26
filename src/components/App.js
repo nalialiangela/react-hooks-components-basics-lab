a43a7c3eddd3debcf0c2e834cd4c62400374a775
naliali <nalialiangela365@gmail.com>
2024-04-26 09:14:24 +0300

    initial basics

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+//import home from "./home"
+//import Nav from "./Nav"
 
 function NavBar() {
   return (
@@ -17,12 +19,22 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+      <p>This is About us.</p>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home/>
+      <About/>
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
